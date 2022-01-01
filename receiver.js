87c43eb8d6013c2fcc87a4d45771c6b412b90a95
cabcallaway <46686431+cabcallaway@users.noreply.github.com>
2022-01-01 15:17:06 -0600

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -1,3 +1,17 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 context.start();
+
+var generateRandomString = function (length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;

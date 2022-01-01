fbeee27dabe7204c9f9a7a86b236c73d8f07f096
cabcallaway <46686431+cabcallaway@users.noreply.github.com>
2022-01-01 15:35:41 -0600

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -2,16 +2,9 @@ const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 context.start();
 
-var generateRandomString = function (length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+// Debug Logger
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+const LOG_TAG = 'MyAPP.LOG';
 
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
-const access_token = 'BQAayZAZaMkaEv20_g0x_XoOOe7EkldylZqLnCSSWg5Trd6G_8c9Z9lqJLrB0vNsRhFU9qXvV8TES0_lRVNgHkP27Gz8aOPSghEJ2jXBs9BdvOtVYk4dq0UTreBgxDd0AqEfDRLIXpQKl22e55ctaaj9YwbH0Oram0Q'
-const script = document.createElement("script");
-    script.src = "https://sdk.scdn.co/spotify-player.js";
-    script.async = true;
+// Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
+castDebugLogger.setEnabled(true);

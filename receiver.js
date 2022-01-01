fea001380dbde651d5c5b603127a4f04f3ed9698
cabcallaway <46686431+cabcallaway@users.noreply.github.com>
2022-01-01 15:36:55 -0600

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -8,3 +8,7 @@ const LOG_TAG = 'MyAPP.LOG';
 
 // Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
 castDebugLogger.setEnabled(true);
+castDebugLogger.loggerLevelByEvents = {
+  'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,
+  'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
+}
